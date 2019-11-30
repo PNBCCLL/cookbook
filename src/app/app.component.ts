2f31adb61c1f8733074b208fd4b9cf0982f32ef6
PNB <pablobeccalli@gmail.com>
2019-11-30 19:12:21 -0300

    testing develop branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cookbook';
+  title = 'Cook Book';
+  dev = 'PNBCCLL';
 }
